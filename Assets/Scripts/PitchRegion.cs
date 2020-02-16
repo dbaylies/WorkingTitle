b1d505af60231879ed5e165d1e1b53811393068a
David Baylies <dbaylies@eventide.com>
2020-02-15 22:21:35 -0500

    Make pitch regions highlight when controller passes through them. Good visual feedback (to be refined)

--- Assets/Scripts/PitchRegion.cs
+++ Assets/Scripts/PitchRegion.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class PitchRegion : MonoBehaviour
 {
+    private void Awake()
+    {
+        SetAlpha(0.1f);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Controller (right)")
         {
-            // SoundManager.Instance.PitchChanged(); ?
             GameManager.Instance.VibrateRightController();
+            SetAlpha(0.8f);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        SetAlpha(0.1f);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        Color regionColor = GetComponent<Renderer>().material.color;
+        regionColor.a = alpha;
+        GetComponent<Renderer>().material.color = regionColor;
     }
 }
